20fbf7006bc8db2f8e0b683c0e0817a582170a1e
UGing265 <duongquocthai368@gmail.com>
2026-06-18 03:17:30 +0700

    feat(compare): add QuestPDF package and set community license

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -6,8 +6,11 @@ using GUI.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using QuestPDF.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
+QuestPDF.Settings.License = LicenseType.Community;
 
 // Add services to the container.
 builder.Services.AddRazorPages();
