e35c026247b5c89bb3fe7fa36f3af8b37acc7029
UGing265 <duongquocthai368@gmail.com>
2026-07-13 08:56:43 +0700

    feat: implement token usage monitoring dashboard and add system documentation files

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -105,17 +105,6 @@ app.MapHub<SystemHub>("/systemHub");
 
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<DAL.Data.DBContext>();
-    dbContext.Database.ExecuteSqlRaw(@"
-        CREATE TABLE IF NOT EXISTS ""SystemSettings"" (
-            ""Key"" character varying(100) NOT NULL,
-            ""Value"" character varying(1000) NOT NULL,
-            ""Description"" character varying(500),
-            ""UpdatedAt"" timestamp with time zone NOT NULL,
-            CONSTRAINT ""PK_SystemSettings"" PRIMARY KEY (""Key"")
-        );
-    ");
-
     var cfg = scope.ServiceProvider.GetRequiredService<IConfiguration>();
     var smtpServer = cfg["SmtpSettings:Server"];
     var senderEmail = cfg["SmtpSettings:SenderEmail"];
